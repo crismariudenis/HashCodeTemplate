6ef29700ccb6eab6d406cb3034192c99bda73131
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-01-22 21:50:54 +0200

    ups

--- input.cpp
+++ input.cpp
@@ -1,31 +1,8 @@
 #include "input.h"
 
 void Input::read(std::string fileName)
-{
-    ifstream cin(fileName);
-    int nrClients;
-    cin >> nrClients;
-    clients.resize(nrClients);
-    for (int i = 0; i < nrClients; i++)
-    {
-        Client client;
-        int x;
-        cin >> x;
-        while (x--)
-        {
-            string ingredient;
-            cin >> ingredient;
-            client.likes[ingredient] = 1;
-            ingredients.insert(ingredient);
-        }
-        cin >> x;
-        while (x--)
-        {
-            string ingredient;
-            cin >> ingredient;
-            client.dislikes[ingredient] = 1;
-            ingredients.insert(ingredient);
-        }
-        clients[i] = client;
-    }
+{  
+    /*
+        Code here
+    */
 }
--- input.h
+++ input.h
@@ -1,18 +1,9 @@
 #include "utils/bits.h"
 
 using namespace std;
-struct Client
-{
-    map<string, bool> likes;
-    map<string, bool> dislikes;
-};
 
 class Input
 {
-
 public:
-    vector<Client> clients;
-    set<string> ingredients;
-
     void read(string fileName);
 };
