8eeba487d953b55d2e3f49c8cb145268bdd25901
crismariudenis <78813212+crismariudenis@users.noreply.github.com>
2023-01-29 21:15:24 +0200

    src folder

--- input.cpp
+++
@@ -1,8 +0,0 @@
-#include "input.h"
-
-void Input::read(std::string fileName)
-{  
-    /*
-        Code here
-    */
-}
--- input.h
+++
@@ -1,9 +0,0 @@
-#include "utils/bits.h"
-
-using namespace std;
-
-class Input
-{
-public:
-    void read(string fileName);
-};
